325ce3ecb26c0d0b292815b91590030b86c0cab7
robochat <none@none>
2015-09-30 18:34:08 +0200

    Adding memoize and reify decorators to module. memoize is from https://wiki.python.org/moin/PythonDecoratorLibrary (works on functions, methods and classes). reify is from the pyramid library.

--- build_system3.py
+++ build_system3.py
@@ -40,6 +40,50 @@ def argmax(lst):
 def argmin(lst):
   return lst.index(min(lst))
 
+def memoize(obj):
+    cache = obj.cache = {}
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = obj(*args, **kwargs)
+        return cache[key]
+    return memoizer
+
+
+class reify(object):
+    """ Use as a class method decorator.  It operates almost exactly like the
+    Python ``@property`` decorator, but it puts the result of the method it
+    decorates into the instance dict after the first call, effectively
+    replacing the function it decorates with an instance variable.  It is, in
+    Python parlance, a non-data descriptor.  An example:
+    .. code-block:: python
+       class Foo(object):
+           @reify
+           def jammy(self):
+               print('jammy called')
+               return 1
+    And usage of Foo:
+    >>> f = Foo()
+    >>> v = f.jammy
+    'jammy called'
+    >>> print(v)
+    1
+    >>> f.jammy
+    1
+    >>> # jammy func not called the second time; it replaced itself with 1
+    """
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        functools.update_wrapper(self, wrapped)
+
+    def __get__(self, inst, objtype=None):
+        if inst is None:
+            return self
+        val = self.wrapped(inst)
+        setattr(inst, self.wrapped.__name__, val)
+        return val
+
 @memorize
 get_mtime(fpath):
     os.path.getmtime(fpath)
