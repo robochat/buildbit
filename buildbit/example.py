6470cd1891061665bc3302ba059cff3d2d3da756
robochat <none@none>
2015-11-12 11:46:30 +0100

    changes to (new) example1.py

--- buildbit/example.py
+++ buildbit/example.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 """example script for package buildbit."""
 from bob import Rule as rule
-import bob
+#import bob
 from sh import touch
 
-r1 = rule('tests/test1.txt',None)
+rule('tests',None,func=['mkdir','{targets}'])
+
+r1 = rule('tests/test1.txt',None,order_only='tests')
 r2 = rule('tests/test2.txt',None)
 r3 = rule('tests/test3.txt',None)
 
-@rule('tests/ex1.txt','tests/test*.txt')
+@rule(['tests/foo.txt','tests/foo2.txt'],'tests/test*.txt',shared=True)
 @rule('tests/ex%.txt','tests/test%.txt')
 def mytouch(self):
     touch(self.targets)
@@ -16,10 +18,12 @@ def mytouch(self):
 for r in r1,r2,r3:
     r.func = mytouch
 
-if __name__=="__main__":
-    rule.main()
+rule('All',['tests','tests/foo2.txt','tests/ex2.txt'],PHONY=True)
 
-# Can easily run buildbit from within script. 
+if __name__=="__main__":
+    rule.main() #supplies a simple commandline interface
+
+# Can alternatively easily run buildbit from within script. 
 if False: #__name__=="__main__":
     buildseq = rule.calc_build('All')
     print buildseq
