c207f65345ee4946ca16166f7ce55819c07296b7
robochat <none@none>
2015-11-12 11:33:02 +0100

    updating example2.py

--- buildbit/example2.py
+++ buildbit/example2.py
@@ -1,23 +1,28 @@
-
-
+#!/usr/bin/env python
+"""example script for package buildbit."""
 from bob import Rule as rule
 import bob
 from sh import touch
 
-
 r1 = rule('tests/test1.txt',None)
 r2 = rule('tests/test2.txt',None)
 r3 = rule('tests/test3.txt',None)
-r4 = rule('tests/ex1.txt','tests/test*.txt')
-r5 = rule('tests/ex%.txt','tests/test%.txt')
 
+@rule('tests/ex1.txt','tests/test*.txt')
+@rule('tests/ex%.txt','tests/test%.txt')
 def mytouch(self):
     touch(self.targets)
 
-for r in r1,r2,r3,r4,r5:
+for r in r1,r2,r3:
     r.func = mytouch
-    
-print rule.calc_build('tests/ex1.txt')
 
+if __name__=="__main__":
+    rule.main()
 
-arule = bob.ExplicitRule.rules['tests/ex1.txt']
+# Can easily run buildbit from within script. 
+if False: #__name__=="__main__":
+    buildseq = rule.calc_build('All')
+    print buildseq
+    response=raw_input("run build? [(y)es/(n)o]")
+    if response in ('y','yes'):
+        rule.build(buildseq)
